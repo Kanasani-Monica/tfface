e2b89ff2de720a3a1dfc3e5d72c429e9a826dfd7
look4pritam <pritam.shete@gmail.com>
2018-05-24 16:22:32 +0530

    Initial commit.

--- scripts/datasets/MS-Celeb-1M/AlignedFaceImages/evaluate_development_dataset.py
+++ scripts/datasets/MS-Celeb-1M/AlignedFaceImages/evaluate_development_dataset.py
@@ -76,12 +76,6 @@ from preprocessing import preprocessing_factory
 
 from tfface.classifier.Classifier import Classifier
 
-def to_rgb(img):
-    w, h = img.shape
-    ret = np.empty((w, h, 3), dtype=np.uint8)
-    ret[:, :, 0] = ret[:, :, 1] = ret[:, :, 2] = img
-    return ret
-
 def main(args):
 	network_size = 299
 
